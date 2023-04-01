11ad2ce79439ea59ec495bc9479ac8e75634de0f
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-04-01 13:22:13 +0900

    Change version 0.2.0-dev
    開発用バージョンに変更

--- kibela.el
+++ kibela.el
@@ -5,7 +5,7 @@
 ;; Author: mugijiru <106833+mugijiru@users.noreply.github.com>
 ;; Maintainer: mugijiru <106833+mugijiru@users.noreply.github.com>
 ;; URL: https://github.com/mugijiru/emacs-kibela
-;; Version: 0.1.0
+;; Version: 0.2.0-dev
 ;; Package-Requires: ((emacs "26.1") (graphql "0.1.1") (request "0.3.3") (markdown-mode "2.5"))
 ;; Keywords: kibela, tools
 
