1308fea907ed1ea11576ea56fb437bebcabbda8e
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-04-06 07:51:52 +0900

    test: Setup for CodeClimate

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,4 +1,7 @@
 (when (require 'undercover nil t)
-  (undercover "*.el"))
+  (undercover "*.el"
+              (:send-report nil)
+              (:report-format 'simplecov)
+              (:report-file "tmp/rspec_results.json")))
 
 (require 'kibela)
